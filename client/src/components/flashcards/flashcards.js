79342ffd6d9ccef1d34c555c64721ebfb4b0be70
olivias1082 <olivia.singletary@selu.edu>
2021-10-19 19:48:11 -0500

    Shows words on cards but does not flip

--- client/src/components/flashcards/flashcards.js
+++ client/src/components/flashcards/flashcards.js
@@ -1,56 +1,100 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Flashcard } from './flashcard'
+import React, { Component, useState } from 'react'
 import axios from 'axios'
-const FlashCards = () => {
-  const [currentIndex, setCurrentIndex] = useState(0)
+import './flashcard.css';
+import './flashcards.css'
+const Flashcard = (record, isFront) => {
+const words = record.word;
+const translations = record.word_translation;
+return (
+
+  <div className="card-container">
+  <div className={record ? "card visibility" : "card"}>
+   
+    <div className={isFront ? "back card-face " : "front card-face"}>
+      <div className={isFront ? "words" : "translations"}>{isFront ? words : translations}</div>
+    </div>
+  </div>
+</div>
+)
+}
+
+const FlashCards = (props) => {
   const [isFront, setIsFront] = useState(true)
-  const[records] = useState([]);
-  useEffect(() => {
-    getFlashcards()
-  }, )
-  
+
   const handleCardFlip = () => {
     setIsFront(!isFront)
   }
-  async function getFlashcards() {
-    try {
-      const res = await axios.get('http://localhost5000/record')
-      console.log(res)
-    
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  const handleNextCard = () => {
-    setCurrentIndex((currentIndex + 1) % records.length)
-    setIsFront(true)
-  }
-
-  const handlePreviousCard = () => {
-    setCurrentIndex((currentIndex - 1 + records.length) % records.length )
-    setIsFront(true)
-  }
-
-
-  const record = records && records.length && records[currentIndex]
 
   return (
     <div>
 
       <div>
-          //<div className="progress"  align = "center">{currentIndex + 1}/{records.length}</div>
           <div onClick={handleCardFlip} align = "center">
-            <Flashcard record={record} isFront={isFront}/>
+            <Flashcard record={props.record} isFront={isFront} word={props.word} word_translation={props.word_translation}/>
           </div>
       </div>
-      <div className="btn-container" align = "center">
-        <button className="btn" onClick={handlePreviousCard}>Previous</button>
-        <button className="btn" onClick={handleNextCard}>Next</button>
-      </div>
+
 
     </div>
   )
 }
 
-export default FlashCards
+export default class Flashcards extends Component {
+  // This is the constructor that shall store our data retrieved from the database
+  constructor(props) {
+    super(props);
+    this.deleteTranslation = this.deleteTranslation.bind(this);
+    this.state = { records: [] };
+  }
+ 
+  // This method will get the data from the database.
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/record/")
+      .then((response) => {
+        this.setState({ records: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+ 
+  // This method will delete a record based on the method
+  deleteTranslation(id) {
+    axios.delete("http://localhost:5000/" + id).then((response) => {
+      console.log(response.data);
+    });
+ 
+    this.setState({
+      record: this.state.records.filter((el) => el._id !== id),
+    });
+  }
+ 
+  // This method will map out the users on the table
+  recordList() {
+    return this.state.records.map((currentrecord) => {
+      return (
+        <FlashCards
+          record={currentrecord}
+          word = {currentrecord.word}
+          word_translation ={currentrecord.word_translation}
+          />
+      );
+    });
+  }
+ 
+  // This following section will display the table with the records of individuals.
+  render() {
+    return (
+
+      <div>
+<div className = "space">
+  <p></p>
+</div>
+        <h3 align="center">Flashcards</h3>
+    {this.recordList()}
+       
+      </div>
+    );
+  }
+}
